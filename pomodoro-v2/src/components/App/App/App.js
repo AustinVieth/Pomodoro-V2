547ca1f8ce294e8d52f499116bb72e03ef4c3e81
Austin Vieth <ozwaldvieth@gmail.com>
2020-06-09 09:48:05 -0700

    format grid, apply base colors

--- pomodoro-v2/src/components/App/App/App.js
+++ pomodoro-v2/src/components/App/App/App.js
@@ -5,16 +5,47 @@ import Clock from "../../Clock/Clock";
 import TaskForm from "../../TaskForm/TaskForm";
 import Navigation from "../../Navigation/Navigation";
 import TaskList from "../../TaskList/TaskList";
+import { getByDisplayValue } from "@testing-library/react";
+
+const style = {
+  display: "grid",
+  gridTemplateColumns: "5fr 3fr",
+  gridTemplateRows: "9fr 8fr 8fr 40fr 14fr 21fr",
+  gridTemplateAreas: `
+  "title navigation"
+  "blank tasklist"
+  "blank tasklist"
+  "clock tasklist"
+  "clock tasklist"
+  "taskform totaltime"
+  `,
+  maxHeight: "calc(100vh - 12.03vh)",
+};
 
 const App = (props) => {
   return (
-    <div class="app">
-      POMODORO
-      <Navigation />
-      <Clock />
-      <TaskForm />
-      <TaskList />
-      <div>Total Time Remaining: 04:20:30</div>
+    <div class="app" style={style}>
+      <h1 style={{ gridArea: "title" }}>POMODORO</h1>
+
+      <div style={{ gridArea: "navigation" }}>
+        <Navigation />
+      </div>
+
+      <div style={{ gridArea: "tasklist" }}>
+        <TaskList />{" "}
+      </div>
+
+      <div style={{ gridArea: "clock" }}>
+        <Clock />
+      </div>
+
+      <div style={{ gridArea: "taskform" }}>
+        <TaskForm />
+      </div>
+
+      <div style={{ gridArea: "totaltime" }}>
+        Total Time Remaining: 04:20:30
+      </div>
     </div>
   );
 };
