78a63ba6da29d68fb8ef16021738ce3103b78eed
Austin Vieth <ozwaldvieth@gmail.com>
2020-06-12 12:33:23 -0700

    add delete button to category

--- pomodoro-v2/src/components/DropDown/Item.js
+++ pomodoro-v2/src/components/DropDown/Item.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 const Item = (props) => {
+  const deleteStyle = {
+    justifySelf: "flex-end",
+    width: "fit-content",
+    transition: "all 0.2s ease-in-out",
+  };
+
   return (
     <li onClick={() => props.selectItem(props.item.id)} className="dd-item">
-      {props.item.title}
+      <div className="title">{props.item.title}</div>
+      <div className="delete" style={deleteStyle}>
+        <i className="fas fa-trash-alt"></i>
+      </div>
     </li>
   );
 };
